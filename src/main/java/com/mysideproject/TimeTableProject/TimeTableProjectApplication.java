6136ad57a0673839ea405e68a2d292c89005f9a9
CJ <choijun0627@konkuk.ac.kr>
2024-04-02 22:42:18 +0900

    Init : 사이드 프로젝트 생성
    사이드 프로젝트인 Time Table Project를 위한 프로젝트 생성

---
+++ src/main/java/com/mysideproject/TimeTableProject/TimeTableProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mysideproject.TimeTableProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTableProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTableProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysideproject/TimeTableProject/TimeTableProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysideproject.TimeTableProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTableProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
