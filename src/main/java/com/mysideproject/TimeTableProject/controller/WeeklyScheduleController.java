8c2c2e734f0716870f6311edeabc1e21c98bae25
CJ <choijun0627@konkuk.ac.kr>
2024-04-08 22:00:46 +0900

    Feat : 미니 프로젝트 컨트롤러 구현
    /timetable로 요청 왔을 때 컨트롤러 showFirstWeeklySchedule 구현
    weeklyScheduleService에서 initWeeklySchedule 메서드 호출해
    WeeklySchedule 반환
    WeeklySchedule model에 넣어서 반환

--- src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
+++ src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
@@ -1,15 +1,22 @@
 package com.mysideproject.TimeTableProject.controller;
 
+import com.mysideproject.TimeTableProject.domain.WeeklySchedule;
+import com.mysideproject.TimeTableProject.service.WeeklyScheduleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class WeeklyScheduleController {
+
+    private final WeeklyScheduleService weeklyScheduleService;
 
     @GetMapping("/timetable")
     public String showFirstWeeklySchedule(Model model) {
-
+        WeeklySchedule weeklySchedule = weeklyScheduleService.initWeeklySchedule();
+        model.addAttribute("weeklySchedule", weeklySchedule);
         return "timetable/weeklySchedule";
     }
 }
