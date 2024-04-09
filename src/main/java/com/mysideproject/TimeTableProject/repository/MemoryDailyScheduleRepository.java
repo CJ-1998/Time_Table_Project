ba2a978b2ba9607115912e3bd60f86ee691c7b49
CJ <choijun0627@konkuk.ac.kr>
2024-04-10 07:14:29 +0900

    Feat : 메모리 일간 계획표 저장소 구현
    메모리에 일간 계획표 저장하는 MemoryDailyScheduleRepository 클래스 구현
    일간 계획표 객체 저장하는 자료구조로 HashMap 사용함
    HashMap의key는 일간 계획표 날짜

---
+++ src/main/java/com/mysideproject/TimeTableProject/repository/MemoryDailyScheduleRepository.java
@@ -0,0 +1,26 @@
+package com.mysideproject.TimeTableProject.repository;
+
+import com.mysideproject.TimeTableProject.domain.DailySchedule;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemoryDailyScheduleRepository implements DailyScheduleRepository {
+    // 일간 계획표 저장하는 Map. key는 일간 계획표의 날짜.
+    private static final Map<LocalDate, DailySchedule> dailyScheduleRepository = new HashMap<>();
+
+    // 일간 계획표 저장 메서드
+    @Override
+    public DailySchedule save(DailySchedule dailySchedule) {
+        dailyScheduleRepository.put(dailySchedule.getDate(), dailySchedule);
+        return dailySchedule;
+    }
+
+    // 특정 날짜와 일치하는 일간 계획표 검색하는 메서드
+    @Override
+    public DailySchedule findByDate(LocalDate date) {
+        return dailyScheduleRepository.get(date);
+    }
+}
