248021d9014d02b69cdc62346c48d961e2689a14
CJ <choijun0627@konkuk.ac.kr>
2024-04-10 07:56:27 +0900

    Feat : 주간 계획표 추가 요청 컨트롤러 구현
    전체 주간 계획표 화면에서 추가 버튼 누르면 요청 보내도록 구현 예정
    이 추가 요청을 잡는 컨트롤러인 addTimeTable 메서드 구현
    그리고 timetable/newTimeTable.html로 이동하도록 반환함
    이 html은 날짜를 선택해서 요청 보내 주간 계획표 생성하게 할 예정

--- src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
+++ src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
@@ -6,12 +6,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class WeeklyScheduleController {
 
     private final WeeklyScheduleService weeklyScheduleService;
+
+    @PostMapping("/timetable/new")
+    public String addTimeTable() {
+        return "timetable/newTimeTable";
+    }
 
     @GetMapping("/timetable")
     public String showFirstWeeklySchedule(Model model) {
