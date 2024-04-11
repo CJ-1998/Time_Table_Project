375125a60803db4014151df67b4eac0a4c1829e2
CJ <choijun0627@konkuk.ac.kr>
2024-04-11 10:27:50 +0900

    Feat : 주간 계획표 컨트롤러 수정
    전체 주간 계획표 화면 요청 받는 컨트롤러 수정
    주간 계획표 저장소에서 전체 주간 계획표 찾아 Model에 담도록 수정
    
    주간 계획표 저장소에 전체 찾는 findAll 메서드 구현 필요

--- src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
+++ src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
@@ -1,7 +1,9 @@
 package com.mysideproject.TimeTableProject.controller;
 
 import com.mysideproject.TimeTableProject.domain.WeeklySchedule;
+import com.mysideproject.TimeTableProject.repository.WeeklyScheduleRepository;
 import com.mysideproject.TimeTableProject.service.WeeklyScheduleService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,14 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class WeeklyScheduleController {
 
     private final WeeklyScheduleService weeklyScheduleService;
+    private final WeeklyScheduleRepository weeklyScheduleRepository;
 
     // 전체 주간 계획표 화면 달라는 요청
-    // 전체 주간 계획표 화면으로 이동동
+    // 전체 주간 계획표 화면으로 이동
     @GetMapping("/timetables")
-    public String showAllWeeklySchedule() {
+    public String showAllWeeklySchedule(Model model) {
+        List<WeeklySchedule> allWeeklySchedule = weeklyScheduleRepository.findAll();
+        model.addAttribute(allWeeklySchedule);
         return "timetable/allWeeklySchedule";
     }
-
 
     // 전체 주간 계획표 화면에서 추가 버튼 누르면 들어오는 요청.
     // 주간 계획표 추가 화면으로 이동.
