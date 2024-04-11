c9833abe16c6df2aa50b205b84fb5f00c8eeae09
CJ <choijun0627@konkuk.ac.kr>
2024-04-11 15:30:02 +0900

    Feat : 주간 계획표 화면 보여주는 컨트롤러 구현
    전체 주간 계획표 화면에서 링크 누르면 startDate를 요청에 담아 보내게 함
    그래서 전체 주간 계획표 화면의 링크에 th:href 속성 추가
    그 후 이 요청을 잡는 showWeeklySchedule 컨트롤러 구현
    @PathVariable로 startDate 받아서 이것 가지고 주간 계획표 저장소에서 찾음
    찾은 주간 계획표를 Model에 담아 주간 계획표 화면으로 보냄

--- src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
+++ src/main/java/com/mysideproject/TimeTableProject/controller/WeeklyScheduleController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,10 +36,22 @@ public class WeeklyScheduleController {
         return "timetable/addWeeklySchedule";
     }
 
+    // 주간 계획표 추가 화면에서 전송 버튼 누르면 들어오는 요청.
+    // 날짜 보내서 이 날짜 가지고 새로운 주간 계획표 생성 후 전체 주간 계획표 화면으로 리다이렉트.
     @PostMapping("/timetable/add")
     public String addTimeTable(@RequestParam("date") LocalDate date) {
         WeeklySchedule weeklySchedule = weeklyScheduleService.initWeeklySchedule(date);
         return "redirect:/timetables";
+    }
+
+    // 전체 주간 계획표 화면에서 링크 클릭 시 들어오는 요청
+    // 요청에 startDate 붙여서 요청 보냄
+    // 같이 온 startDate를 가지고 주간 계획표 찾아서 Model에 담아 주간 계획표 화면으로 이동.
+    @GetMapping("/timetable/{startDate}")
+    public String showWeeklySchedule(@PathVariable("startDate") LocalDate startDate, Model model) {
+        WeeklySchedule weeklySchedule = weeklyScheduleRepository.findByStartDate(startDate);
+        model.addAttribute("weeklySchedule", weeklySchedule);
+        return "timetable/weeklySchedule";
     }
 
     // 비어 있는 주간 계획표를 보여주는 컨트롤러.
