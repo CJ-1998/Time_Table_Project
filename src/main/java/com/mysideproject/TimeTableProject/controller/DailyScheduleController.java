b04f884665aca9b9c82f88916baa1ad83b52a9d9
CJ <choijun0627@konkuk.ac.kr>
2024-04-23 15:57:32 +0900

    Feat : 일간 계획표 화면, 컨트롤러 구현
    일간 계획표 화면 구현
    계획, 실행, 체크리스트 있는 일간 계획표 화면 구현
    일간 계획표 화면 요청 받는 컨트롤러 구현
    넘어온 날짜의 일간 계획표 찾아와 Model에 담아 넘기도록 구현

--- src/main/java/com/mysideproject/TimeTableProject/controller/DailyScheduleController.java
+++ src/main/java/com/mysideproject/TimeTableProject/controller/DailyScheduleController.java
@@ -1,15 +1,24 @@
 package com.mysideproject.TimeTableProject.controller;
 
+import com.mysideproject.TimeTableProject.domain.DailySchedule;
+import com.mysideproject.TimeTableProject.repository.DailyScheduleRepository;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class DailyScheduleController {
 
+    private final DailyScheduleRepository dailyScheduleRepository;
+
     @GetMapping("/timetable/dailySchedule/{date}")
-    public String showDailySchedule() {
-        return null;
+    public String showDailySchedule(@PathVariable("date") LocalDate date, Model model) {
+        DailySchedule dailySchedule = dailyScheduleRepository.findByDate(date);
+        model.addAttribute("dailySchedule", dailySchedule);
+        return "timetable/dailySchedule";
     }
 }
