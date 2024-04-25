884d707b00ddccc7815629c7823e4e419c934325
CJ <choijun0627@konkuk.ac.kr>
2024-04-25 11:42:43 +0900

    Feat : 시간 계산 로직 일부 구현
    시간 계산 로직에서 일간 계획표 찾아서 PlanContentTime으로 계산
    PlanContentTime으로 계산하는 메서드 구현 필요

--- src/main/java/com/mysideproject/TimeTableProject/service/TimeCalculationService.java
+++ src/main/java/com/mysideproject/TimeTableProject/service/TimeCalculationService.java
@@ -1,7 +1,11 @@
 package com.mysideproject.TimeTableProject.service;
 
+import com.mysideproject.TimeTableProject.domain.DailySchedule;
 import com.mysideproject.TimeTableProject.domain.PlanContentTime;
+import com.mysideproject.TimeTableProject.domain.WeeklySchedule;
 import com.mysideproject.TimeTableProject.repository.WeeklyScheduleRepository;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +16,22 @@ public class TimeCalculationService {
 
     private final WeeklyScheduleRepository weeklyScheduleRepository;
 
-    public List<PlanContentTime> calculateWeeklyScheduleTime() {
+    public List<PlanContentTime> calculateWeeklyScheduleTime(LocalDate startDate) {
+        List<PlanContentTime> planContentTimes = new ArrayList<>();
+
+        WeeklySchedule weeklySchedule = weeklyScheduleRepository.findByStartDate(startDate);
+        List<DailySchedule> weeklyPlans = weeklySchedule.getWeeklyPlans();
+
+        for (DailySchedule dailySchedule : weeklyPlans) {
+            PlanContentTime planContentTime = calculateDailyScheduleTime(dailySchedule);
+            planContentTimes.add(planContentTime);
+        }
+
+        return planContentTimes;
+    }
+
+    private PlanContentTime calculateDailyScheduleTime(DailySchedule dailySchedule) {
+
         return null;
     }
 }
